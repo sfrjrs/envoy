7226c95782173bec3f13fc0d95f45079bc20f0cf
Jason Spence <json.spence@gmail.com>
2016-10-29 19:32:59 -0700

    add carousel images. add responsive styles to slide 1 and 2

--- scripts/main.js
+++ scripts/main.js
@@ -19,10 +19,11 @@ $(window).resize(function() {
   }
 });
 
-$(document).ready(function() {
+$(function() {
+  
+  // Menu
   var menuToggle = $("#js-mobile-menu").unbind();
   var navMenu = $("#js-navigation-menu");
-
   $(navMenu).removeClass("show");
   menuToggle.on("click", function(e) {
     e.preventDefault();
@@ -32,4 +33,62 @@ $(document).ready(function() {
       }
     });
   });
-}); 
+
+  // Carousel
+  var carouselEle = $(".hero-slider");
+  var carouselDotsBtnWrap = $(".hero-slider-navdots-wrap");
+  var carouselPrevNextBtnWrap = $(".hero-slider-nav-wrap");
+  var carouselDotsBtnEle = ".slick-dots";
+  $(carouselEle).slick({
+      infinite: false,
+      dots: true,
+      appendDots: carouselDotsBtnWrap,
+      prevArrow: "<i class='fa fa-angle-left fa-3x' aria-hidden='true'></i>",
+      nextArrow: "<i class='fa fa-angle-right fa-3x' aria-hidden='true'></i>",
+      appendArrows: carouselPrevNextBtnWrap,
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: 'unslick'
+        }
+      ]
+  });
+
+  $(carouselEle).on('beforeChange', function(event, slick, currentSlide, nextSlide){
+      appendClassToDots(carouselDotsBtnEle, nextSlide);
+  });
+
+});
+
+function appendClassToDots(dotsEle, slide) {
+  var dotNavEle = $(dotsEle);
+  switch (slide) {
+    case 0:
+      $(dotsEle)
+        .removeClass('vizio escape ultra')
+        .addClass('django');
+      break;
+    case 1:
+      $(dotsEle)
+        .removeClass('django escape ultra')
+        .addClass('vizio');
+      break;
+    case 2:
+      $(dotsEle)
+        .removeClass('django vizio ultra')
+        .addClass('escape');
+      break;
+    case 3:
+      $(dotsEle)
+        .removeClass('django vizio escape')
+        .addClass('ultra');
+      break;
+  }
+};
